dfeb77ad4efbbf7ca9fdf83bc62076c0826dd37d
KevinRichardDev <kevin.richard0@orange.fr>
2024-02-05 05:10:10 +0100

    fetchFlag

--- index.js
+++ index.js
@@ -7,8 +7,18 @@
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+// coutry.name.includes(inputSearch.value);
 
 // 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
 
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+
+let flags = [];
+
+async function fetchFlag() {
+  await fetch('https://restcountries.com/v3.1/all')
+    .then((res) => res.json())
+    .then((data) => (flags = data.name));
+
+  console.log(flags);
+}
